d0270b2ddc677096df9692a46cffb08f34faf0b9
WuMaxtw <WuMax13@gmail.com>
2023-04-10 20:01:29 +0800

    BST

--- Binary_Search_Tree/BinarySearchTree.h
+++ Binary_Search_Tree/BinarySearchTree.h
@@ -20,8 +20,10 @@ private:
 public:
     Node():root(0){};
     Node *Search(int k);
-    void Insert(string data, int k);
-    void Inorder();
+    void Insert(string d, int k);
+    void Inorder(Node *cur);
+    void Preorder(Node *cur);
+    void Postorder(Node *cur);
 };
 
 Node* BST::Search(int k){
@@ -33,6 +35,49 @@ Node* BST::Search(int k){
             cur = cur->rightchild;
     }
     return cur;
+}
+
+void BST::Insert(string d, int k){
+    Node *p = nullptr, *c = root;
+    Node *cur = nullptr;
+    Node *insert_node = new Node(k, d);
+    while(cur != nullptr){
+        p = cur;
+        if(insert_node->key < cur->key)
+            cur = cur->leftchild;
+        else
+            cur = cur->rightchild
+    }
+    insert_node->parent = p;
+    if(p == nullptr)
+        this->root = insert_node;
+    else if(insert_node->key < p->key)
+        p->leftchild = insert_node;
+    else
+        p->rightchild = insert_node;
+}
+
+void BST::Inorder(Node *cur){ // LVR
+    if(cur){
+        Inorder(cur->leftchild);
+        cout << cur->data << ' ';
+        Inorder(cur->rightchild);
+    }
+}
+void BST::Preorder(Node *cur){ // VLR
+    if(cur){
+        cout << cur->data << ' ';
+        Preorder(cur->leftchild);
+        Preorder(cur->rightchild);
+    }
+}
+
+void BST::Postorder(Node *cur){ //LRV
+    if(cur){
+        Postorder(cur->leftchild);
+        Postorder(cur->rightchld);
+        cout << cur->data << ' ';
+    }
 }
 
 #endif
--- Binary_Search_Tree/main.cpp
+++ Binary_Search_Tree/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "BinarySearchTree.h"
 int main(){
+    BST BSTree;
+
+    BSTree.InsertBST(8,"龜仙人");
+    BSTree.InsertBST(1000,"悟空");
+    BSTree.InsertBST(2,"克林");
+    BSTree.InsertBST(513,"比克");
+
+    cout << "Inorder Traversal:" << '\n';
+    BSTree.Inorder();
+    cout << '\n';
     
 
     return 0;
