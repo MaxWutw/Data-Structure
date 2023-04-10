84c987ab271e2771b15064b476b7cce2e17215ed
WuMaxtw <WuMax13@gmail.com>
2023-04-10 23:33:13 +0800

    BST

--- Binary_Search_Tree/BinarySearchTree.h
+++ Binary_Search_Tree/BinarySearchTree.h
@@ -1,6 +1,17 @@
 #ifndef BinarySearchTree_h
 #define BinarySearchTree_h
+#include <iostream>
+#include <string>
+#include <queue>
 
-
+class TreeNode{
+private:
+    TreeNode *leftchild, *rightchild, *parent;
+    int key;
+    std::string data;
+public:
+    TreeNode():leftchild(nullptr), rightchild(nullptr), parent(nullptr), key(0), data(""){};
+    TreeNode(int k, string inp_data):leftchild(nullptr), rightchild(nullptr), parent(nullptr), key(k), data(inp_data){};
+};
 
 #endif
